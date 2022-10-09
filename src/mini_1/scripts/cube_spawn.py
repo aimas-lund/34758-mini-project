681826c059a2a3f9e3579d7879396216bc8bb16b
Aimas Lund <33122600+aimas-lund@users.noreply.github.com>
2022-10-09 13:36:49 +0200

    Added refresh mode

--- src/mini_1/scripts/cube_spawn.py
+++ src/mini_1/scripts/cube_spawn.py
@@ -1,50 +1,82 @@
 #!/usr/bin/env python
 
-import rospy, tf, random
+import rospy, tf, random, os
 import tf_conversions
-from gazebo_msgs.srv import DeleteModel, SpawnModel, GetModelState
+from gazebo_msgs.srv import DeleteModel, SpawnModel, GetModelState, GetWorldProperties
 from geometry_msgs.msg import *
 from pubobject import publish_object
 
-def spawn_cubes():
-    print("Waiting for gazebo services...")
+class Spawner:
 
-    #Remove .init_node if calling function from somewhere else where a node has been initiated.
-    #rospy.init_node("spawn_products_in_bins")
-    rospy.wait_for_service("gazebo/delete_model")
-    rospy.wait_for_service("gazebo/spawn_sdf_model")
-    print("Got it.")
-    delete_model = rospy.ServiceProxy("gazebo/delete_model", DeleteModel)
-    spawn_model = rospy.ServiceProxy("gazebo/spawn_sdf_model", SpawnModel)
-    model_coordinates = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)
+    _DIR_NAME = os.path.dirname(__file__)
+    _WORLD = "world"
+    _CUBE_NAME = "cube"
+    _BUCKET_NAME = "bucket"
 
-    with open("./urdf/cube.urdf", "r") as f:
-        product_xml = f.read()
+    _ORIENT = Quaternion(*tf_conversions.transformations.quaternion_from_euler(0., 0.0, 0.785398))
+    _BUCKET_POSE = Pose(Point(x=0.53, y=-0.23, z=0.78), _ORIENT)
 
-    orient = Quaternion(*tf_conversions.transformations.quaternion_from_euler(0., 0.0, 0.785398))
+    def __init__(self):
+        print("Waiting for gazebo services...")
+        rospy.wait_for_service("gazebo/delete_model")
+        rospy.wait_for_service("gazebo/spawn_sdf_model")
 
-    num_of_cubes = random.randint(2,6)
+        print("Initializing rospy service proxies...")
+        self.delete_model = rospy.ServiceProxy("gazebo/delete_model", DeleteModel)
+        self.spawn_model = rospy.ServiceProxy("gazebo/spawn_sdf_model", SpawnModel)
+        self.model_coordinates = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)
+        self.world_properties = rospy.ServiceProxy('/gazebo/get_world_properties', GetWorldProperties)
 
-    for num in xrange(0,num_of_cubes):
-        bin_y   =   random.uniform(0,0.5)
-        bin_x   =   random.uniform(0,0.5)
-        item_name   =   "cube{}".format(num)
-        print("Spawning model:%s", item_name)
-        item_pose   =   Pose(Point(x=bin_x, y=bin_y,    z=1),   orient)
-        spawn_model(item_name, product_xml, "", item_pose, "world")
 
-        #Publish cube
-        publish_object(item_name,item_pose)
+    def spawn_models(self):
+        self.spawn_cubes()
+        self.spawn_bucket()
 
-    with open("./urdf/bucket.urdf", "r") as f:
-        product_xml = f.read()
+        
+    def spawn_cubes(self):
+        cube_urdf_path = os.path.join(self._DIR_NAME, "../urdf/cube.urdf")
+        with open(cube_urdf_path, "r") as f:
+            product_xml = f.read()
 
-    item_pose   =   Pose(Point(x=0.53, y=-0.23,    z=0.78),   orient)
-    print("Spawning model:%s", "bucket")
-    spawn_model("bucket", product_xml, "", item_pose, "world")
-    #Publish bucket
-    publish_object("bucket",item_pose)
+        num_of_cubes = random.randint(2,6)
+
+        for num in xrange(0,num_of_cubes):
+            bin_y   =   random.uniform(0,0.5)
+            bin_x   =   random.uniform(0,0.5)
+            item_name   =   "{}{}".format(self._CUBE_NAME, num)
+
+            print("Spawning model:%s", item_name)
+            item_pose   =   Pose(Point(x=bin_x, y=bin_y,    z=1),   self._ORIENT)
+            self.spawn_model(item_name, product_xml, "", item_pose, self._WORLD)
+
+            #Publish cube
+            publish_object(item_name,item_pose)
+
+
+    def spawn_bucket(self, pose=_BUCKET_POSE):
+        bucket_urdf_path = os.path.join(self._DIR_NAME, "../urdf/bucket.urdf")
+        with open(bucket_urdf_path, "r") as f:
+            product_xml = f.read()
+
+        print("Spawning model:%s", self._BUCKET_NAME)
+        self.spawn_model(self._BUCKET_NAME, product_xml, "", pose, self._WORLD)
+        #Publish bucket
+        publish_object(self._BUCKET_NAME, pose)
+
+
+    def despawn_cubes(self):
+        model_list = self.world_properties()
+        for model in model_list.model_names:
+            if self._CUBE_NAME not in model:
+                continue
+
+            try:
+                self.delete_model(str(model))
+            except:
+                print("Could not find model '{}' in Gazebo".format(str(model)))
 
 
 if __name__ == '__main__':
-    spawn_cubes()
+    spawner = Spawner()
+    spawner.spawn_cubes()
+    spawner.spawn_bucket()
