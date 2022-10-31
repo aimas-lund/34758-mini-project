3670c6ae4ca23ceee84fe58391c57178c5963bff
Aimas Lund <aimas.lund@gmail.com>
2022-10-31 15:09:00 +0100

    MVP navigator class

--- src/final_project/scripts/navigator.py
+++ src/final_project/scripts/navigator.py
@@ -5,13 +5,21 @@ from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 class Navigator:
 
     _MAP_FRAME_ID = 'map'
+    _ROSPY_NODE_NAME = 'navigator'
 
     _waypoints = [  
         [(1.13, -1.6, 0.0), (0.0, 0.0, -0.16547, -0.986213798314)],
-        [(0.13, 1.93, 0.0), (0.0, 0.0, -0.64003024, -0.76812292098)]
+        [(0.0, 0.0, 0.0), (0.0, 0.0, -0.64003024, -0.76812292098)]
     ]
 
-    def move_to_pose(self, pose, frame_id=_MAP_FRAME_ID):  
+    def __init__(self):
+        rospy.init_node(self._ROSPY_NODE_NAME)
+        self.client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
+        self.client.wait_for_server()
+        print('--- Navigator module initialized ---')
+
+
+    def _goal_pose(self, pose, frame_id=_MAP_FRAME_ID):  
         goal_pose = MoveBaseGoal()
         goal_pose.target_pose.header.frame_id = frame_id
         goal_pose.target_pose.pose.position.x = pose[0][0]
@@ -25,16 +33,20 @@ class Navigator:
         return goal_pose
 
 
+    def move_to_pose(self, pose, frame_id=_MAP_FRAME_ID):
+        goal = self._goal_pose(pose)
+        self.client.send_goal(goal)
+        print('Moving to pose:')
+        print(pose)
+        
+        self.client.wait_for_result()
+        rospy.sleep(3)
+
+        return
+
 if __name__ == '__main__':
-    navigator = Navigator()
-    rospy.init_node('patrol')
- 
-    client = actionlib.SimpleActionClient('move_base', MoveBaseAction) 
-    client.wait_for_server()
-   
+    nav = Navigator()
+
     while True:
-        for pose in navigator._waypoints:   
-            goal = navigator.move_to_pose(pose, 'map')
-            client.send_goal(goal)
-            client.wait_for_result()
-            rospy.sleep(3)
+        for pose in nav._waypoints:   
+            nav.move_to_pose(pose)
